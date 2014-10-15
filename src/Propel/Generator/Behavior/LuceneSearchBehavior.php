c26b83b15327e20234fa20c8e2d68e4a59f38658
rswebdev <robert@rswebdev.de>
2014-10-15 14:14:56 +0200

    Added missing changes in LuceneSearchBehavior.php

--- src/Propel/Generator/Behavior/LuceneSearchBehavior.php
+++ src/Propel/Generator/Behavior/LuceneSearchBehavior.php
@@ -1,6 +1,7 @@
 <?php
 namespace RSWebDev\Propel\Generator\Behavior;
 
+use Propel\Generator\Behavior\I18n\I18nBehavior;
 use RSWebDev\Propel\Generator\Behavior\LuceneSearchBehavior\LuceneSearchBehaviorObjectBuilderModifier;
 use RSWebDev\Propel\Generator\Behavior\LuceneSearchBehavior\LuceneSearchBehaviorQueryBuilderModifier;
 use Propel\Generator\Model\Behavior;
@@ -9,10 +10,21 @@ class LuceneSearchBehavior extends Behavior {
 
     protected $objectBuilderModifier;
     protected $queryBuilderModifier;
+    public $i18n;
 
     protected $parameters = array(
         'columns'      => '',
     );
+
+    public function replaceTokens($string)
+    {
+        $table = $this->getTable();
+
+        return strtr($string, array(
+            '%TABLE%'   => $table->getOriginCommonName(),
+            '%PHPNAME%' => $table->getPhpName(),
+        ));
+    }
 
     public function getSearchColumns()
     {
@@ -20,7 +32,25 @@ class LuceneSearchBehavior extends Behavior {
         $table = $this->getTable();
         if ($columnNames = $this->getSearchColumnNamesFromConfig()) {
             foreach ($columnNames as $columnName) {
-                $columns []= $table->getColumn($columnName);
+                $_column = $table->getColumn($columnName);
+                if ($_column != null)
+                    $columns []= $_column;
+            }
+        }
+        $this->i18n = false;
+        if (empty($columns)) {
+            $other_behaviors = $table->getBehaviors();
+            foreach ($other_behaviors as $other_behavior) {
+                if ($other_behavior instanceof I18nBehavior) {
+                    $this->i18n = true;
+                    $database = $this->getTable()->getDatabase();
+                    $i18n_table = $database->getTable($this->replaceTokens($other_behavior->getParameter('i18n_table')));
+                    foreach ($columnNames as $columnName) {
+                        $_column = $i18n_table->getColumn($columnName);
+                        if ($_column != null)
+                            $columns []= $_column;
+                    }
+                }
             }
         }
         return $columns;
