62c535294786fd952cd130d56b80e034b6aadd0e
rswebdev <robert@rswebdev.de>
2014-10-15 08:47:05 +0200

    PSR-0

---
+++ src/Propel/Generator/Behavior/LuceneSearchBehavior.php
@@ -0,0 +1,57 @@
+<?php
+namespace RSWebDev\Propel\Generator\Behavior;
+
+use RSWebDev\Propel\Generator\Behavior\LuceneSearchBehavior\LuceneSearchBehaviorObjectBuilderModifier;
+use RSWebDev\Propel\Generator\Behavior\LuceneSearchBehavior\LuceneSearchBehaviorQueryBuilderModifier;
+use Propel\Generator\Model\Behavior;
+
+class LuceneSearchBehavior extends Behavior {
+
+    protected $objectBuilderModifier;
+    protected $queryBuilderModifier;
+
+    protected $parameters = array(
+        'columns'      => '',
+    );
+
+    public function getSearchColumns()
+    {
+        $columns = array();
+        $table = $this->getTable();
+        if ($columnNames = $this->getSearchColumnNamesFromConfig()) {
+            foreach ($columnNames as $columnName) {
+                $columns []= $table->getColumn($columnName);
+            }
+        }
+        return $columns;
+    }
+
+    protected function getSearchColumnNamesFromConfig()
+    {
+        $columnNames = explode(',', $this->getParameter('columns'));
+        foreach ($columnNames as $key => $columnName) {
+            if ($columnName = trim($columnName)) {
+                $columnNames[$key] = $columnName;
+            } else {
+                unset($columnNames[$key]);
+            }
+        }
+        return $columnNames;
+    }
+
+    public function getObjectBuilderModifier()
+    {
+        if (is_null($this->objectBuilderModifier)) {
+            $this->objectBuilderModifier = new LuceneSearchBehaviorObjectBuilderModifier($this);
+        }
+        return $this->objectBuilderModifier;
+    }
+
+    public function getQueryBuilderModifier()
+    {
+        if (is_null($this->queryBuilderModifier)) {
+            $this->queryBuilderModifier = new LuceneSearchBehaviorQueryBuilderModifier($this);
+        }
+        return $this->queryBuilderModifier;
+    }
+}
